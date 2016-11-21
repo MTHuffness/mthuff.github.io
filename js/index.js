3015c3dbc4dc721ff20bd3cf6ad1d47b50b3e6b6
MTHuffness <MTHuffcode@gmail.com>
2016-11-21 12:21:57 -0700

    v4

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.mainDiv').fadeIn(1400);
-    $('.linkDiv').effect('slide', 'right', 1400);
+    $('.linkDiv').animate({width:'show'}, 1350);
     $('.linkDiv').hover(
         function() {
             $(this).removeClass("linkDiv").addClass("linkDivHover");
