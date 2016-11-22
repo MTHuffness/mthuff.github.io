891fa3f7e2a45a3151c2175d9c4aa8fa30a66725
MTHuffness <MTHuffcode@gmail.com>
2016-11-21 17:20:09 -0700

    v5

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $('.mainDiv').fadeIn(1400);
     $('.linkDiv').animate({width:'show'}, 1350);
+    $('.rightTriangle').delay(1350).fadeIn(1200);
     $('.linkDiv').hover(
         function() {
             $(this).removeClass("linkDiv").addClass("linkDivHover");
+            $(this).find('.rightTriangle').fadeOut(200).fadeIn(200).removeClass('rightTriangle').addClass('rightTriangle2');
     }, function() {
             $(this).removeClass("linkDivHover").addClass("linkDiv");
+            $(this).find('.rightTriangle2').removeClass('rightTriangle2').addClass('rightTriangle');
     });
     $('.linkDiv').click(function() {
             $(this).effect('bounce',{times:1}, 200).removeClass("linkDivHover").addClass("linkDivClick");
