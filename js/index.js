89e4f0bb2ab3a0d915f10bcde8e50ded6d601f85
MTHuffness <MTHuffcode@gmail.com>
2016-11-29 16:51:53 -0700

    logo baby!!!

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+
+//ANIMATIONS AT THE START OF THE PAGE LOAD----------------------------------
     $('.mainDiv').fadeIn(1400);
     $('.grid').animate({width:'show'}, 1350);
 
+//BUTTON FUNCTIONALITY - CLICK TO SHOW PAGE---------------------------------
     $('#servicesSide').click(function() {
         $('#portfolioPage, #aboutPage, #contactPage').hide();
+        $('header').animate({ 
+            opacity: 0
+        }, 600, function() {
+            $('#headerTitle').text('Services');
+            $('header').animate({opacity: 1}, 600);
+        });
         $('.mainDiv').animate({
             opacity: .5,
             right: 1200}, 600, function() {
                 $('#servicesPage').show();
-            }).animate({right: 0}, 800).animate({opacity: 1}, 600);
+        }).animate({right: 0}, 800).animate({opacity: 1}, 600);
     });
 
     $('#portfolioSide').click(function() {
