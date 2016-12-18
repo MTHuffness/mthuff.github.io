5f04ce05c0b65d57df2844ac5b5fb040c5679b69
MTHuffness <MTHuffcode@gmail.com>
2016-12-18 16:00:08 -0700

    mobile menu added v1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+
     $('#menu').click(function() {
-        $('nav').slideDown(1000);
+        dropDown();
     });
+
+function dropDown() {
+        $('#menu').hide(1000);
+        $('#linkMenu').slideDown(1000);
+        $('.link').addClass('mobileLink');
+        $('.link').removeClass('link linkCurrent');
+        $('#menuClose').fadeIn(1000);
+        $('header').css('margin-bottom', '40%');
+};
+
+    $('#menuClose').click(function() {
+        $(this).slideUp(1000);
+        $('#linkMenu').delay(1000).slideUp(1000);
+        $('#menu').fadeIn(800);  
+        $('header').css('margin-bottom', '0');
+    })
 });
