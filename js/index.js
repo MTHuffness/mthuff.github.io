54ee76264cb27ed9dde6dc968b532509f139ba79
MTHuffness <MTHuffcode@gmail.com>
2016-12-21 18:05:56 -0700

    added animated home page box

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         dropDown();
     });
 
-function dropDown() {
+    function dropDown() {
         $('#menu').hide(1000);
         $('#linkMenu').slideDown(1000);
         $('.link').addClass('mobileLink');
@@ -18,5 +18,100 @@ function dropDown() {
         $('#linkMenu').delay(1000).slideUp(1000);
         $('#menu').fadeIn(800);  
         $('header').css('margin-bottom', '0');
-    })
+    });
+
+//TRANSITIONS FOR EACH BUTTON
+    //BOX 1 BUTTONS
+    $('.next1').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(-90deg)'
+        });
+        $('#triangle li:nth-child(2)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(1)').css({
+            opacity: .25
+        });    
+    });
+
+    $('.prev1').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(90deg)'
+        });
+        $('#triangle li:nth-child(4)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(1)').css({
+            opacity: .25
+        });    
+    });
+    //BOX 2 BUTTONS
+    $('.next2').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(-180deg)'
+        });
+        $('#triangle li:nth-child(3)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(2)').css({
+            opacity: .25
+        });    
+    });
+    $('.prev2').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(0deg)'
+        });
+        $('#triangle li:nth-child(1)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(2)').css({
+            opacity: .25
+        });    
+    });
+    //BOX 3 BUTTONS
+    $('.next3').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(-270deg)'
+        });
+        $('#triangle li:nth-child(4)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(3)').css({
+            opacity: .5
+        });    
+    });
+    $('.prev3').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(270deg)'
+        });
+        $('#triangle li:nth-child(2)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(3)').css({
+            opacity: .5
+        });    
+    });
+    //BOX 4 BUTTONS
+    $('.next4').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(0deg)'
+        });
+        $('#triangle li:nth-child(1)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(4)').css({
+            opacity: .5
+        });    
+    });
+    $('.prev4').click(function() {
+        $('#triangle').css({
+            transform: 'translateZ(-470px) rotateY(180deg)'
+        });
+        $('#triangle li:nth-child(3)').css({
+            opacity: 1
+        });
+        $('#triangle li:nth-child(4)').css({
+            opacity: .5
+        });    
+    });
 });
